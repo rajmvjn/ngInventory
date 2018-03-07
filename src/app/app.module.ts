c59b22796a7eb76f8563b7bac972b233392dfd0e
Rajesh Vijayan <A-8076@ibsplc.com>
2018-03-07 16:37:50 +0530

    auth module added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AuthComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AuthComponent]
 })
 export class AppModule { }
---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: '../auth/auth.component.html',
+  styleUrls: ['../auth/auth.component.css']
+})
+export class AuthComponent {
+  title = 'RR Inventory';
+}
